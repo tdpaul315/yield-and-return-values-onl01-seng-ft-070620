b9f0fe8ece4e6855fba9a4bfbf1f1222c6e791f8
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-19 22:30:01 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,11 +1,13 @@
+'require pry'
 
 def hello(array)
   i = 0
-  while i < array.length 
+  while i < array.length
+  binding.pry
 yield(array[i])
     i += 1
   end
 end
 
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
